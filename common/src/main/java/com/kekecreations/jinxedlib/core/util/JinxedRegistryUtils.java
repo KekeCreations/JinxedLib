36025a2ac7f03aeb96937697aae33fed21c6c8f8
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2025-01-05 07:34:29 +0000

    More Registry Utils

--- common/src/main/java/com/kekecreations/jinxedlib/core/util/JinxedRegistryUtils.java
+++ common/src/main/java/com/kekecreations/jinxedlib/core/util/JinxedRegistryUtils.java
@@ -1,5 +1,6 @@
 package com.kekecreations.jinxedlib.core.util;
 
+import com.kekecreations.jinxedlib.JinxedLib;
 import com.kekecreations.jinxedlib.core.mixin.SpriteSourcesMixin;
 import com.kekecreations.jinxedlib.core.mixin.WoodTypeMixin;
 import com.kekecreations.jinxedlib.core.platform.Services;
@@ -7,13 +8,24 @@ import com.mojang.serialization.MapCodec;
 import net.minecraft.client.renderer.texture.atlas.SpriteSource;
 import net.minecraft.client.renderer.texture.atlas.SpriteSourceType;
 import net.minecraft.core.Registry;
+import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.grower.TreeGrower;
 import net.minecraft.world.level.block.state.properties.WoodType;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.storage.loot.LootTable;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 public class JinxedRegistryUtils {
 
@@ -53,10 +65,52 @@ public class JinxedRegistryUtils {
 
     /**
      * This method allows you to register custom wood types
-     * @param woodType your custom wood type
+     * @param woodType Your custom wood type
      */
     public static WoodType registerWoodType(WoodType woodType) {
         return WoodTypeMixin.invokeRegister(woodType);
+    }
+
+    /**
+     * This method allows you to register custom tree growers
+     * @param modID Your mod identifier
+     * @param name Your tree grower's name
+     * @param tree Optional tree feature
+     * @param secondaryTree Optional secondary tree feature
+     */
+    public static TreeGrower register(String modID, String name, Optional<ResourceKey<ConfiguredFeature<?, ?>>> tree, Optional<ResourceKey<ConfiguredFeature<?, ?>>> secondaryTree) {
+        return register(modID, name, 0, Optional.empty(), Optional.empty(), tree, secondaryTree, Optional.empty(), Optional.empty());
+    }
+
+    private static TreeGrower register(String modID, String name, float secondaryChance, Optional<ResourceKey<ConfiguredFeature<?, ?>>> megaTree, Optional<ResourceKey<ConfiguredFeature<?, ?>>> secondaryMegaTree, Optional<ResourceKey<ConfiguredFeature<?, ?>>> tree, Optional<ResourceKey<ConfiguredFeature<?, ?>>> secondaryTree, Optional<ResourceKey<ConfiguredFeature<?, ?>>> flowers, Optional<ResourceKey<ConfiguredFeature<?, ?>>> secondaryFlowers) {
+        return new TreeGrower(String.format("%s:%s", modID, name), secondaryChance, megaTree, secondaryMegaTree, tree, secondaryTree, flowers, secondaryFlowers);
+    }
+
+
+
+    private static final Set<ResourceKey<LootTable>> LOCATIONS = new HashSet();
+    private static final Set<ResourceKey<LootTable>> IMMUTABLE_LOCATIONS;
+
+
+    /**
+     * This method allows you to register custom-built in loot tables
+     * @param modID Your mod identifier
+     * @param name Name of your built-in loot table (can include paths like gameplay/lotus_flower_harvest)
+     */
+    public static ResourceKey<LootTable> registerBuiltInLootTable(String modID, String name) {
+        return register(ResourceKey.create(Registries.LOOT_TABLE, JinxedLib.customId(modID, name)));
+    }
+
+    private static ResourceKey<LootTable> register(ResourceKey<LootTable> $$0) {
+        if (LOCATIONS.add($$0)) {
+            return $$0;
+        } else {
+            throw new IllegalArgumentException(String.valueOf($$0.location()) + " is already a registered built-in loot table");
+        }
+    }
+
+    public static Set<ResourceKey<LootTable>> all() {
+        return IMMUTABLE_LOCATIONS;
     }
 
 
@@ -68,5 +122,9 @@ public class JinxedRegistryUtils {
      */
     public static SpriteSourceType registerSpriteSource(String id, MapCodec<? extends SpriteSource> codec) {
         return SpriteSourcesMixin.invokeRegister(id, codec);
+    }
+
+    static {
+        IMMUTABLE_LOCATIONS = Collections.unmodifiableSet(LOCATIONS);
     }
 }
