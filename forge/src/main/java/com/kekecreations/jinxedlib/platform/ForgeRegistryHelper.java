f0065068defeedaced8498fae07b85d5425224ed
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2025-06-04 06:58:31 +0100

    Fixed ForgeRegistryHelper

--- forge/src/main/java/com/kekecreations/jinxedlib/platform/ForgeRegistryHelper.java
+++ forge/src/main/java/com/kekecreations/jinxedlib/platform/ForgeRegistryHelper.java
@@ -1,13 +1,13 @@
 package com.kekecreations.jinxedlib.platform;
 
 import com.kekecreations.jinxedlib.core.platform.services.IRegistryHelper;
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistry;
 import net.minecraftforge.registries.RegistryManager;
-import net.minecraftforge.registries.RegistryObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,28 +19,23 @@ public class ForgeRegistryHelper implements IRegistryHelper {
 
     @Override
     public <T> Supplier<T> register(Registry<? super T> registry, String modID, String name, Supplier<T> entry) {
-        return registryMap.register(registry, modID, name, entry);
+        return registryMap.register(modID, registry, name, entry);
     }
 
 
     private static class RegistryMap {
 
-        private final Map<ResourceLocation, DeferredRegister<?>> registries = new HashMap<>();
+        private final Map<Pair<String, ResourceLocation>, DeferredRegister<?>> registries = new HashMap<>();
 
         @SuppressWarnings({"unchecked", "rawtypes"})
-        private <T> RegistryObject<T> register(Registry<? super T> registry, String modID, String name, Supplier<T> entry) {
-            DeferredRegister<T> reg = (DeferredRegister<T>)registries.computeIfAbsent(registry.key().location(), (key) -> {
-                ForgeRegistry forgeReg = RegistryManager.ACTIVE.getRegistry(key);
-
-                if(forgeReg == null)
-                    return null;
-
-                DeferredRegister<T> defReg = DeferredRegister.create(forgeReg, modID);
+        private <T> Supplier<T> register(String modID, Registry<? super T> registry, String name, Supplier<T> entry) {
+            DeferredRegister<T> reg = (DeferredRegister<T>)registries.computeIfAbsent(Pair.of(modID, registry.key().location()), (key) -> {
+                ForgeRegistry forgeReg = RegistryManager.ACTIVE.getRegistry(key.getSecond());
+                if (forgeReg == null) return null;
+                DeferredRegister<T> defReg = DeferredRegister.create(forgeReg, key.getFirst());
                 defReg.register(FMLJavaModLoadingContext.get().getModEventBus());
-
                 return defReg;
             });
-
             return reg != null ? reg.register(name, entry) : null;
         }
 
