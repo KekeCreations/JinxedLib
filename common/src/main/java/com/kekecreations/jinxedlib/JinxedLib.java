71284bb45ee2ed89f1f9e436a8b9719aea11ffa1
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2025-06-05 23:25:26 +0100

    Build Time!

--- common/src/main/java/com/kekecreations/jinxedlib/JinxedLib.java
+++ common/src/main/java/com/kekecreations/jinxedlib/JinxedLib.java
@@ -16,7 +16,7 @@ public class JinxedLib {
         JinxedDatapackRegistries.register();
 
 
-        JinxedRegistryExamples.loadExampleClass();
+        //JinxedRegistryExamples.loadExampleClass();
     }
 
     /**
